acf86845a570f3e17870ad7e06a74c0481380d42
felipeperassi <fperassi@fi.uba.ar>
2026-06-14 01:40:23 -0300

    Refactor main.py to update path imports and remove unused source coding function; add source_coding.py for Huffman encoding implementation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from data.config import PATH, OUTPUT_PATH, MEDIA_PATH
+from data.config import TXT_PATH, OUTPUT_PATH, MEDIA_PATH
 from scripts.transmitter import (
     appearence_probs, entropy, huffman_algorithm, mean_length, minimum_length, shannon_range, codificate_text, 
     modulate_symbols, calculate_energies,codificate_channel
@@ -7,46 +7,6 @@ from scripts.transmitter import (
 from scripts.receiver import decode_text, write_file, code_parameters, decodificate_channel, syndrome_table, parity, demodulate_symbols, symbol_error_probability, bit_error_probability
 from scripts.extras import plot_char_counts, print_dict, plot_constellation
 from scripts.channel import channel_effects
-
-#CODIFICACION DE FUENTE
-
-def codificacion_fuente(text, return_codified=False):
-    # Calculate probabilities & counts
-    probs_dict, char_counts_dict = appearence_probs(text)
-    print_dict(char_counts_dict, "Character counts:", sort=True)
-    print_dict(probs_dict, "Appearance probabilities:", sort=True)
-    plot_char_counts(char_counts_dict, MEDIA_PATH)
-    
-    # Huffman coding
-    code_dict = huffman_algorithm(probs_dict)
-    print_dict(code_dict, "Huffman codes:", sort=True)
-
-    # Calculate entropy, Shannon range, mean and minimum code lengths
-    entropy_value = entropy(probs_dict)
-    shannon_rng = shannon_range(entropy_value)
-    print(f"Entropy: {entropy_value:.3f} bits/symbol",
-        f"Shannon range: {shannon_rng[0]:.3f} to {shannon_rng[1]:.3f} bits/symbol", sep="\n")
-
-    mean_len, min_len = mean_length(code_dict, probs_dict), minimum_length(code_dict)
-    print(f"Mean code length: {mean_len:.3f} bits/symbol", 
-           f"Minimum code length: {min_len:.3f} bits/symbol", 
-           f"Efficiency: {entropy_value / mean_len:.3f}", sep="\n")
-
-    # Codification and decoding
-    codified_text = codificate_text(text, code_dict)
-    decoded_text = decode_text(codified_text, code_dict)
-    write_file(OUTPUT_PATH / "decoded_output.txt", decoded_text)
-
-    # Sentence to test the codification and decoding
-    text_sentence = text.split('.')[1][1::] + '.' # take the first sentence of the text, removing the leading space and adding the dot at the end
-    codified_sentence = codificate_text(text_sentence, code_dict)
-    decoded_sentence = decode_text(codified_sentence, code_dict)
-    print(f"\nOriginal sentence: {text_sentence}", 
-          f"Codified sentence: {' '.join(codified_sentence)}", 
-          f"Decoded sentence: {''.join(decoded_sentence)}", sep="\n")
-    
- 
-    if return_codified: return codified_text
 
 #CODIFICACION DE CANAL
 
@@ -124,13 +84,13 @@ def modulacion(binary_vector):
 
 
 if __name__ == "__main__":
-    with open(PATH, 'r') as f:
+    with open(TXT_PATH, 'r') as f:
         text = f.read()
     
-    codified_text = codificacion_fuente(text, return_codified=True)
-    binary_vector = np.array([int(b) for b in ''.join(codified_text)]) # Vector of bits representing the codified text
-    encoded_vector = codificacion_canal(binary_vector)
-    modulacion(encoded_vector)
+    # codified_text = codificacion_fuente(text, return_codified=True)
+    # binary_vector = np.array([int(b) for b in ''.join(codified_text)]) # Vector of bits representing the codified text
+    # encoded_vector = codificacion_canal(binary_vector)
+    # modulacion(encoded_vector)
 
     
     
---
+++ test/source_coding.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+from data.config import OUTPUT_PATH, MEDIA_PATH, TXT_PATH
+from scripts.transmitter import (
+    appearence_probs, entropy, huffman_algorithm, mean_length, 
+    minimum_length, shannon_range, codificate_text
+)
+from scripts.receiver import decode_text, write_file
+from scripts.extras import plot_char_counts, print_dict
+    
+def source_coding(text : str) -> None:
+    """
+    Performs source encoding & decoding using Huffman coding for a given text.
+    
+    Parameters:
+        text: str, the input text to be encoded and decoded
+    """
+
+    # Calculate probabilities & counts
+    probs_dict, char_counts_dict = appearence_probs(text)
+    print_dict(char_counts_dict, "Character counts:", sort=True)
+    print_dict(probs_dict, "Appearance probabilities:", sort=True)
+    plot_char_counts(char_counts_dict, MEDIA_PATH)
+    
+    # Huffman coding
+    code_dict = huffman_algorithm(probs_dict)
+    print_dict(code_dict, "Huffman codes:", sort=True)
+
+    # Calculate entropy, Shannon range, mean and minimum code lengths
+    entropy_value = entropy(probs_dict)
+    shannon_rng = shannon_range(entropy_value)
+    print(f"Entropy: {entropy_value:.3f} bits/symbol",
+        f"Shannon range: {shannon_rng[0]:.3f} to {shannon_rng[1]:.3f} bits/symbol", sep="\n")
+
+    mean_len, min_len = mean_length(code_dict, probs_dict), minimum_length(code_dict)
+    print(f"Mean code length: {mean_len:.3f} bits/symbol", 
+           f"Minimum code length: {min_len:.3f} bits/symbol", sep="\n")
+
+    # Codification and decoding
+    codified_text = codificate_text(text, code_dict)
+    decoded_text = decode_text(codified_text, code_dict)
+    write_file(OUTPUT_PATH / "decoded_output.txt", decoded_text)
+
+    # Sentence to test the codification and decoding
+    text_sentence = text.split('.')[1][1::] + '.' # take the first sentence of the text, removing the leading space and adding the dot at the end
+    codified_sentence = codificate_text(text_sentence, code_dict)
+    decoded_sentence = decode_text(codified_sentence, code_dict)
+    print(f"\nOriginal sentence: {text_sentence}", 
+          f"Codified sentence: {' '.join(codified_sentence)}", 
+          f"Decoded sentence: {''.join(decoded_sentence)}", sep="\n")
+
+if __name__ == "__main__":
+    with open(TXT_PATH, 'r') as f:
+        text = f.read()
+    
+    source_coding(text)
